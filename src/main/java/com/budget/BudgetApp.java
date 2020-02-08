31eece3758a732eda3c8a33ee6caa1a712e484ef
Andrew Limas <>
2020-02-08 17:00:36 +0100

    Initial commit

---
+++ src/main/java/com/budget/BudgetApp.java
@@ -0,0 +1,13 @@
+package com.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApp {
+
+	public static void main(String[] args) {
+        SpringApplication.run(BudgetApp.class, args);
+	}
+
+}
